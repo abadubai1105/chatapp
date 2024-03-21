9affa5a6ae9bb1812c3785ace8d04f9765141b93
NGHĨA CÁI HỮU <20120335@student.hcmus.edu.vn>
2024-03-21 13:04:07 +0700

    #1 Nghia: update all user component

--- pages/alluser.js
+++ pages/alluser.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserCard } from "../Components";
+import Style from  "../styles/alluser.module.css";
+import { ChatAppContext } from "../Context/ChatAppContext";
+import 'bootstrap/dist/css/bootstrap.css'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const alluser = () => {
-  return <div>alluser</div>;
+
+  const { userLists, addFriends } = useContext(ChatAppContext);
+
+  return (<div className={Style.all}>
+            <div class="input-group flex-nowrap">
+              <div className={Style.alluser_info}>
+                <h1> List Friends </h1>
+              </div>
+              <div className={Style.search}>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>
+                <button type="button" class="btn btn-outline-dark"><span>Search</span><i class="bi bi-search"></i></button>
+              </div>
+            </div>
+            <div className={Style.alluser}>
+              {userLists.map((el, i)=>
+                <UserCard key ={i+1} el={el} i = {i} addFriends = {addFriends}/>)}            
+            </div>
+          </div>
+  )
 };
 
 export default alluser;
